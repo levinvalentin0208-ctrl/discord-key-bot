8ae80d2290b7a520d03245b9c84871ab0ad68f8a
levinvalentin0208-ctrl <levin.valentin0208@gmail.com>
2026-01-22 20:03:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -238,3 +238,13 @@ client.on("interactionCreate", async interaction => {
 
 /* ================== LOGIN ================== */
 client.login(process.env.DISCORD_TOKEN);
+
+// ===== KEEP ALIVE SERVER =====
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Bot is running");
+}).listen(process.env.PORT || 3000, () => {
+  console.log("🌐 Keep-alive server running");
+});
